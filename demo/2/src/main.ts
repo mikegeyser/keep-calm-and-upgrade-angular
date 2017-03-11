01cfd4673252398a0f0c4d920076bc67560121ea
Mike Geyser <michaelg@bbd.co.za>
2017-03-11 17:36:42 +0200

    Stuffed it up

--- demo/2/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { OldAppModule } from './app/old/app';
-
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-    (<any>ref.instance).upgrade.bootstrap(document.body, [OldAppModule.name]);
-});
